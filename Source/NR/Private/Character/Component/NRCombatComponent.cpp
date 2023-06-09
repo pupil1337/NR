30d45e7b0ac06772083aeafcd11fef3b20396374
pupil1337 <2500698263@qq.com>
2023-06-09 18:02:29 +0800

    初步增加Combat组件

---
+++ Source/NR/Private/Character/Component/NRCombatComponent.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Component/NRCombatComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Character/NRCharacter.h"
+
+
+UNRCombatComponent::UNRCombatComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+}
+
+void UNRCombatComponent::InitLocallyControlledInputEvent(UInputComponent* PlayerInputComponent)
+{
+	Super::InitLocallyControlledInputEvent(PlayerInputComponent);
+
+	if (NRCharacter)
+	{
+		if (const APlayerController* PlayerController = NRCharacter->GetController<APlayerController>())
+		{
+			if (const ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer())
+			{
+				if (UEnhancedInputLocalPlayerSubsystem* EnhancedInputLocalPlayerSubsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+				{
+					if (IMC_Combat)
+					{
+						EnhancedInputLocalPlayerSubsystem->AddMappingContext(IMC_Combat, 0);
+						if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+						{
+							if (IA_Fire)
+							{
+								EnhancedInputComponent->BindAction(IA_Fire, ETriggerEvent::Started, this, &ThisClass::StartFireInput);
+								EnhancedInputComponent->BindAction(IA_Fire, ETriggerEvent::Completed, this, &ThisClass::EndFireInput);
+							}
+							if (IA_Aim)
+							{
+								EnhancedInputComponent->BindAction(IA_Aim, ETriggerEvent::Started, this, &ThisClass::StartAimInput);
+								EnhancedInputComponent->BindAction(IA_Aim, ETriggerEvent::Completed, this, &ThisClass::EndAimInput);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void UNRCombatComponent::StartFireInput()
+{
+}
+
+void UNRCombatComponent::EndFireInput()
+{
+}
+
+void UNRCombatComponent::StartAimInput()
+{
+}
+
+void UNRCombatComponent::EndAimInput()
+{
+}
+
---
+++ Source/NR/Public/Character/Component/NRCombatComponent.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NRComponentBase.h"
+#include "NRCombatComponent.generated.h"
+
+
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS(Abstract, Blueprintable)
+class NR_API UNRCombatComponent : public UNRComponentBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, meta=(AllowPrivateAccess="true"), Category="配置|输入")
+	TObjectPtr<UInputMappingContext> IMC_Combat;
+	UPROPERTY(EditDefaultsOnly, meta=(AllowPrivateAccess="true"), Category="配置|输入")
+	TObjectPtr<UInputAction> IA_Fire;
+	UPROPERTY(EditDefaultsOnly, meta=(AllowPrivateAccess="true"), Category="配置|输入")
+	TObjectPtr<UInputAction> IA_Aim;
+	
+public:
+	UNRCombatComponent();
+
+protected:
+	virtual void InitLocallyControlledInputEvent(UInputComponent* PlayerInputComponent) override;
+
+//~Begin This Class
+private:
+	UFUNCTION()
+	void StartFireInput();
+	
+	UFUNCTION()
+	void EndFireInput();
+
+	UFUNCTION()
+	void StartAimInput();
+
+	UFUNCTION()
+	void EndAimInput();
+};
