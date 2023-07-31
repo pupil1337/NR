a6c867c03d731a255468d227e2b2627701cc71ed
pupil1337 <2500698263@qq.com>
2023-07-31 22:29:13 +0800

    增加UAbilitySystemGlobals::InitGlobalData()

---
+++ Source/NR/Private/Subsystem/NREngineSubsystem.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Subsystem/NREngineSubsystem.h"
+
+#include "AbilitySystemGlobals.h"
+
+void UNREngineSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+
+	UAbilitySystemGlobals::Get().InitGlobalData();
+}
---
+++ Source/NR/Public/Subsystem/NREngineSubsystem.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "NREngineSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NR_API UNREngineSubsystem : public UEngineSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+};
